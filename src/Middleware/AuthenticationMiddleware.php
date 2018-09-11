c50c35f399a896334839885fb9ecd41ab8db2d96
Robert Pustułka <robert.pustulka@gmail.com>
2018-09-11 14:09:13 +0200

    Remove redirection code. This middleware should handle authentication only.

--- src/Middleware/AuthenticationMiddleware.php
+++ src/Middleware/AuthenticationMiddleware.php
@@ -26,25 +26,6 @@ class AuthenticationMiddleware implements MiddlewareInterface
     protected $responseFactory;
 
     /**
-     * Authentication failure redirect URL
-     *
-     * @var string|callable
-     */
-    protected $unauthorizedRedirectUrl;
-
-    /**
-     * Successful login redirect URL
-     *
-     * @var string|callable
-     */
-    protected $successRedirectUrl;
-
-    /**
-     * @var AuthenticationServiceInterface|null
-     */
-    protected $service;
-
-    /**
      * @var AuthenticationServiceProviderInterface
      */
     protected $provider;
@@ -75,59 +56,6 @@ class AuthenticationMiddleware implements MiddlewareInterface
         return $this;
     }
 
-    protected function setRedirect($redirectUrl, $type)
-    {
-        if (!is_string($redirectUrl) && !is_callable($redirectUrl)) {
-            throw new InvalidArgumentException('Redirect URL must be a string or callable');
-        }
-
-        if (!in_array($type, ['unauthorized', 'success'])) {
-            throw new InvalidArgumentException('Type must be failure or success');
-        }
-
-        $property = $type . 'RedirectUrl';
-        $this->{$property} = $redirectUrl;
-    }
-
-    public function setLoginRedirect($redirectUrl): self
-    {
-        $this->setRedirect($redirectUrl, 'success');
-
-        return $this;
-    }
-
-    public function setUnauhtorizedRedirect($redirectUrl): self
-    {
-        $this->setRedirect($redirectUrl, 'unauthorized');
-
-        return $this;
-    }
-
-    protected function getUnauthorizedRedirectResponse($request, $authResult, $authenticator): ResponseInterface
-    {
-        $response = $this->responseFactory->createResponse(302);
-
-        if (is_callable($this->failureDirectUrl)) {
-            return $this->redirectUrl($request, $response, $authResult, $authenticator);
-        }
-
-        return $response->withHeader('Location', $this->redirectUrl);
-    }
-
-    /**
-     *
-     * @param ServerRequestInterface $request
-     * @return AuthenticationServiceInterface
-     */
-    protected function getService(ServerRequestInterface $request): AuthenticationServiceInterface
-    {
-        if ($this->service === null) {
-            $this->service = $this->provider->getAuthenticationService($request);
-        }
-
-        return $this->service;
-    }
-
     /**
      * Process an incoming server request
      *
@@ -137,7 +65,7 @@ class AuthenticationMiddleware implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $service = $this->getService($request);
+        $service = $this->provider->getAuthenticationService($request);
 
         try {
             $wasAuthenticated = $service->authenticate($request);
@@ -184,36 +112,5 @@ class AuthenticationMiddleware implements MiddlewareInterface
         }
 
         return $response;
-    }
-
-    protected function getRedirectResponse(string $targetUrl): ResponseInterface
-    {
-        return $this->responseFactory
-                ->createResponse(302)
-                ->withHeader('Location', $targetUrl);
-    }
-
-    /**
-     * Returns redirect URL.
-     *
-     * @param string $target Redirect target.
-     * @param \Psr\Http\Message\ServerRequestInterface $request Request instance.
-     * @return string
-     */
-    protected function getRedirectUrl(string $target, ServerRequestInterface $request): string
-    {
-        $param = $this->getConfig('queryParam');
-        if ($param === null) {
-            return $target;
-        }
-
-        $query = urlencode($param) . '=' . urlencode($request->getUri());
-        if (strpos($target, '?') !== false) {
-            $query = '&' . $query;
-        } else {
-            $query = '?' . $query;
-        }
-
-        return $target . $query;
     }
 }
