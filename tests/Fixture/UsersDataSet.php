ff8907a9cb2eb914aa385ec50b83e478482afcb8
Florian Krämer <florian.kraemer@kreative-design.net>
2018-09-17 09:34:54 +0200

    Preliminary commit

--- tests/Fixture/UsersDataSet.php
+++ tests/Fixture/UsersDataSet.php
@@ -3,10 +3,12 @@ declare(strict_types=1);
 
 namespace Authentication\Test\Fixture;
 
+use PHPUnit\DbUnit\Database\Table;
 use PHPUnit\DbUnit\DataSet\AbstractDataSet;
 use PHPUnit\DbUnit\DataSet\DefaultTableMetaData;
 use PHPUnit\DbUnit\DataSet\DefaultTable;
 use PHPUnit\DbUnit\DataSet\DefaultTableIterator;
+use PHPUnit\DbUnit\DataSet\ITableIterator;
 
 class UsersDataSet extends AbstractDataSet
 {
@@ -21,23 +23,24 @@ class UsersDataSet extends AbstractDataSet
      * @return array
      */
     public function getData(): array {
-        return [
-            'users' => [
-                'id' => 1,
-                'username' => 'florian',
-                'password' => 'password'
-            ], [
-                'id' => 2,
-                'username' => 'robert',
-                'password' => 'password'
-            ]
+        $data = [
+            ['username' => 'mariano', 'password' => '$2a$10$u05j8FjsvLBNdfhBhc21LOuVMpzpabVXQ9OpC2wO3pSO0q6t7HHMO', 'created' => '2007-03-17 01:16:23', 'updated' => '2007-03-17 01:18:31'],
+            ['username' => 'nate', 'password' => '$2a$10$u05j8FjsvLBNdfhBhc21LOuVMpzpabVXQ9OpC2wO3pSO0q6t7HHMO', 'created' => '2008-03-17 01:18:23', 'updated' => '2008-03-17 01:20:31'],
+            ['username' => 'larry', 'password' => '$2a$10$u05j8FjsvLBNdfhBhc21LOuVMpzpabVXQ9OpC2wO3pSO0q6t7HHMO', 'created' => '2010-05-10 01:20:23', 'updated' => '2010-05-10 01:22:31'],
+            ['username' => 'garrett', 'password' => '$2a$10$u05j8FjsvLBNdfhBhc21LOuVMpzpabVXQ9OpC2wO3pSO0q6t7HHMO', 'created' => '2012-06-10 01:22:23', 'updated' => '2012-06-12 01:24:31']
         ];
+
+        foreach ($data as &$user) {
+            $user['password'] = password_hash('password', PASSWORD_DEFAULT);
+        }
+
+        return $data;
     }
 
     /**
-     * @param array $data
+     * @param array $data Data
      */
-    public function __construct(array $data)
+    public function __construct(array $data = [])
     {
         foreach ($data as $tableName => $rows) {
             $columns = [];
@@ -55,12 +58,18 @@ class UsersDataSet extends AbstractDataSet
         }
     }
 
-    protected function createIterator($reverse = false)
+    /**
+     * @return \PHPUnit\DbUnit\DataSet\ITableIterator
+     */
+    protected function createIterator($reverse = false): ITableIterator
     {
         return new DefaultTableIterator($this->tables, $reverse);
     }
 
-    public function getTable($tableName)
+    /**
+     *
+     */
+    public function getTable($tableName): Table
     {
         if (!isset($this->tables[$tableName])) {
             throw new InvalidArgumentException("$tableName is not a table in the current database.");
