91808dd9e330ad124ca2558eb83db99796f2f3af
Robert Pustułka <robert.pustulka@gmail.com>
2018-09-18 10:03:01 +0200

    Add/change license headers to some of the new files

--- src/Authenticator/Storage/StorageInterface.php
+++ src/Authenticator/Storage/StorageInterface.php
@@ -1,16 +1,14 @@
 <?php
 declare(strict_types=1);
 /**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * Copyright (c) Phauthentic (https://github.com/Phauthentic)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         1.0.0
+ * @copyright     Copyright (c) Phauthentic (https://github.com/Phauthentic)
+ * @link          https://github.com/Phauthentic
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace Authentication\Authenticator\Storage;
