8bb6f24d7b20b8321d2f4cad5450a30afce2d793
Florian Krämer <florian.kraemer@kreative-design.net>
2018-09-19 02:25:10 +0200

    Started on a test case for the Middleware

---
+++ tests/TestCase/Middleware/AuthenticationMiddlewareTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Phauthentic\Authentication\Test\TestCase\Middleware;
+
+use Phauthentic\Authentication\AuthenticationServiceProviderInterface;
+use Phauthentic\Authentication\HttpFactory\ZendDiactoresResponseFactory;
+use Phauthentic\Authentication\HttpFactory\ZendStreamFactory;
+use Phauthentic\Authentication\Middleware\AuthenticationMiddleware;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Authentication Middleware Test
+ */
+class AuthenticationMiddlewareTest extends TestCase
+{
+    /**
+     * testProcess
+     *
+     * @return void
+     */
+    public function testProcess(): void {
+        $service = $this
+            ->getMockBuilder(AuthenticationServiceProviderInterface::class)
+            ->getMock();
+
+        $middleware = new AuthenticationMiddleware(
+            $service,
+            new ZendDiactoresResponseFactory(),
+            new ZendStreamFactory()
+        );
+    }
+}
