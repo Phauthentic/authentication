da0849d92160b4f8304693eafcb5a6bf014172b5
Florian Krämer <florian.kraemer@kreative-design.net>
2018-09-19 15:09:01 +0200

    Adding another test for the middleware

--- tests/TestCase/Middleware/AuthenticationErrorHandlerMiddlewareTest.php
+++ tests/TestCase/Middleware/AuthenticationErrorHandlerMiddlewareTest.php
@@ -25,15 +25,33 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\StreamInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Throwable;
 
-class TestRequestHandler implements RequestHandlerInterface {
+class TestRequestHandler implements RequestHandlerInterface
+{
+    /**
+     * Exception
+     *
+     * @var \Throwable
+     */
+    protected $exception;
+
+    /**
+     * Constructor
+     *
+     * @param \Throwable $exception Exception
+     */
+    public function __construct($exception)
+    {
+        $this->exception = $exception;
+    }
 
     /**
      * Handle the request and return a response.
      */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        throw new UnauthorizedException([], 'Failed :(');
+        throw $this->exception;
     }
 }
 
@@ -42,51 +60,83 @@ class TestRequestHandler implements RequestHandlerInterface {
  */
 class AuthenticationErrorHandlerMiddlewareTest extends TestCase
 {
+    public function setUp()/* The :void return type declaration that should be here would cause a BC issue */ {
+        parent::setUp();
+
+        $this->request = $this
+            ->getMockBuilder(ServerRequestInterface::class)
+            ->getMock();
+
+        $this->response = $this
+            ->getMockBuilder(ResponseInterface::class)
+            ->getMock();
+
+        $this->responseFactory = $this
+            ->getMockBuilder(ResponseFactoryInterface::class)
+            ->getMock();
+
+        $this->streamFactory = $this
+            ->getMockBuilder(StreamFactoryInterface::class)
+            ->getMock();
+
+        $this->stream = $this
+            ->getMockBuilder(StreamInterface::class)
+            ->getMock();
+    }
+
+    /**
+     * testProcessAndCreateErrorResponse
+     *
+     * @return void
+     */
+    public function testProcessAndCreateErrorResponse(): void {
+        $handler = new TestRequestHandler(new UnauthenticatedException('Failed to authenticate :('));
+        $middleware = new AuthenticationErrorHandlerMiddleware($this->responseFactory, $this->streamFactory);
+
+        $this->stream->expects($this->once())
+            ->method('write')
+            ->with('Failed to authenticate :(');
+
+        $this->streamFactory->expects($this->any())
+            ->method('createStream')
+            ->willReturn($this->stream);
+
+        $this->response->expects($this->once())
+            ->method('withBody')
+            ->willReturnSelf();
+
+        $this->responseFactory->expects($this->any())
+            ->method('createResponse')
+            ->willReturn($this->response);
+
+        $middleware->process($this->request, $handler);
+    }
+
     /**
      * testProcessAndCreateUnauthorizedResponse
      *
      * @return void
      */
     public function testProcessAndCreateUnauthorizedResponse(): void {
-        $request = $this
-            ->getMockBuilder(ServerRequestInterface::class)
-            ->getMock();
+        $handler = new TestRequestHandler(new UnauthorizedException([], 'Failed :('));
+        $middleware = new AuthenticationErrorHandlerMiddleware($this->responseFactory, $this->streamFactory);
 
-        $response = $this
-            ->getMockBuilder(ResponseInterface::class)
-            ->getMock();
-
-        $responseFactory = $this
-            ->getMockBuilder(ResponseFactoryInterface::class)
-            ->getMock();
-
-        $streamFactory = $this
-            ->getMockBuilder(StreamFactoryInterface::class)
-            ->getMock();
-
-        $stream = $this
-            ->getMockBuilder(StreamInterface::class)
-            ->getMock();
-
-        $handler = new TestRequestHandler();
-        $middleware = new AuthenticationErrorHandlerMiddleware($responseFactory, $streamFactory);
-
-        $stream->expects($this->once())
+        $this->stream->expects($this->once())
             ->method('write')
             ->with('Failed :(');
 
-        $streamFactory->expects($this->any())
+        $this->streamFactory->expects($this->any())
             ->method('createStream')
-            ->willReturn($stream);
+            ->willReturn($this->stream);
 
-        $response->expects($this->once())
+        $this->response->expects($this->once())
             ->method('withBody')
             ->willReturnSelf();
 
-        $responseFactory->expects($this->any())
+        $this->responseFactory->expects($this->any())
             ->method('createResponse')
-            ->willReturn($response);
+            ->willReturn($this->response);
 
-        $middleware->process($request, $handler);
+        $middleware->process($this->request, $handler);
     }
 }
