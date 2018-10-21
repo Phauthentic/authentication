e2e1e1cbf8fad716136e067ee296f5f582c7d5b7
Florian Krämer <florian.kraemer@kreative-design.net>
2018-10-22 00:47:29 +0200

    Adding a simple php session storage adapter with no deps

---
+++ tests/TestCase/Authenticator/Storage/NativePhpSessionStorageTest.php
@@ -0,0 +1,87 @@
+<?php
+declare(strict_types=1);
+/**
+ * Copyright (c) Phauthentic (https://github.com/Phauthentic)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Phauthentic (https://github.com/Phauthentic)
+ * @link          https://github.com/Phauthentic
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Phauthentic\Authentication\Test\TestCase\Authenticator\Storage;
+
+use Phauthentic\Authentication\Authenticator\Storage\NativePhpSessionStorage;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * NativePhpSessionStorageTest
+ */
+class NativePhpSessionStorageTest extends TestCase
+{
+    /**
+     * Server Request Mock
+     *
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    protected $request;
+
+    /**
+     * Response Mock
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    protected $response;
+
+    /**
+     * @inheritDoc
+     */
+    public function setUp()/* The :void return type declaration that should be here would cause a BC issue */
+    {
+        parent::setUp();
+
+        $this->request = $this->getMockBuilder(ServerRequestInterface::class)
+            ->getMock();
+
+        $this->response = $this->getMockBuilder(ResponseInterface::class)
+            ->getMock();
+    }
+
+    /**
+     * testGetIdentifier
+     *
+     * @return void
+     */
+    public function testReadFromEmptySession()
+    {
+        $storage = new NativePhpSessionStorage('Auth');
+        $result = $storage->read($this->request);
+        $this->assertNull($result);
+    }
+
+    /**
+     * testGetIdentifier
+     *
+     * @return void
+     */
+    public function testWrite()
+    {
+        $data = ['username' => 'florian'];
+        $storage = new NativePhpSessionStorage('Auth');
+
+        $storage->write($this->request, $this->response, $data);
+        $this->assertEquals($_SESSION['Auth'], $data);
+
+        $result = $storage->read($this->request);
+        $this->assertEquals($data, $result);
+
+        $storage->clear($this->request,  $this->response);
+        $result = $storage->read($this->request);
+        $this->assertNull($result);
+    }
+}
